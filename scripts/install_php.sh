7711ae265203a71a9f78244939ef3cb3132e75f2
Ben Kuskopf <kussie@gmail.com>
2021-01-06 14:57:36 +1000

    Fix composer isntall

--- scripts/install_php.sh
+++ scripts/install_php.sh
@@ -2,17 +2,17 @@
 
 if test $(which brew)
 then
-	brew install php@7.4;
 	brew install php@7.3;
+	brew install php@7.4;
 	brew install php@8.0;
 	brew unlink php@7.3;
 	brew unlink php@7.4;
 	brew unlink php@8.0;
 	brew link php@8.0;
-
   echo "Installing Composer";
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 	php composer-setup.php --install-dir=bin --filename=composer;
-	mv composer.phar /usr/local/bin/composer;
+	#mv composer.phar /usr/local/bin/composer;
 else
 	echo "Homebrew Required"
 fi
